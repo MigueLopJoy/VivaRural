9364774007443f3b687997e808e0399ef383c691
Miguel <m.lop.rod.97@gmail.com>
2023-12-19 20:15:06 +0100

    creando ejemplo de site page

---
+++ admin/ASSETS/PHP/LAYERS/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Site administrations</title>
+        <link rel="stylesheet" href="./ASSETS/CSS/general-styles.css">
+        <link rel="stylesheet" href="./ASSETS/CSS/admin-layer.css">
+        <link rel="stylesheet" href="./ASSETS/CSS/pages.css">
+    </head>
+    <body>
+        
+        <div class="program-wrapper">
+            <div class="admin-container box">
+                <nav class="admin-menu">
+                    <ul>
+                        <a href="?"><li class="nav-link search-town">Search</li></a>
+                        <a href="?create-town"><li class="nav-link create-town">Create</li></a> 
+                    </ul>
+                </nav>
+                <div class="admin-window">
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,13 @@
 <?php
+    include "./ASSETS/PHP/LAYERS/layers.php";
+    
+    include "./ASSETS/PHP/LAYERS/header.php";
 
+    if (isset($_GET['create-town'])) {
+        renderCreateTownLayer();
+    } else {
+        renderSearchEngineLayer();
+    }
 
+    include "./ASSETS/PHP/LAYERS/footer.php";
 ?>
